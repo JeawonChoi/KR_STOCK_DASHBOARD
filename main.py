dbd521051324cb2052d2eca317ef762f4de36920
Smart Factory / SCADA / PLC / SQL / DB / Python / R / Grafana / Data Engineer <110613871+JeawonChoi@users.noreply.github.com>
2026-02-25 21:42:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ def process_and_save_html(df, filename="index.html", name_max_width=90):
         <link rel="apple-touch-icon" href="https://cdn-icons-png.flaticon.com/512/2942/2942244.png">
         <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/2942/2942244.png">
         
-        <title>국내 증시 대시보드 앱</title>
+        <title>국내 증시 대시보드</title>
         
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
@@ -332,8 +332,8 @@ def process_and_save_html(df, filename="index.html", name_max_width=90):
         <div class="container-fluid">
             <div class="d-flex justify-content-between align-items-center mb-2">
                 <div class="d-flex align-items-baseline">
-                    <h2 class="fw-bold m-0">국내 주식 대시보드</h2>
-                    <span class="update-time">⏱ 업데이트: {update_time_str}</span>
+                    <h2 class="fw-bold m-0">K-Stock</h2>
+                    <span class="update-time">⏱ Update: {update_time_str}</span>
                 </div>
                 <button id="resetBtn" class="btn btn-outline-light btn-sm" style="min-width: 80px;">
                     <span class="spinner-border spinner-border-sm d-none" id="resetSpinner" role="status" aria-hidden="true"></span>
@@ -598,3 +598,4 @@ if __name__ == "__main__":
     df = get_full_market_data()
     df = merge_treasury_stock(df, 'data.csv')
     process_and_save_html(df, filename="index.html", name_max_width=90)
+
